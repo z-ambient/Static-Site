72bb9381be8172a50e7813486f891e890c3b0af2
zdoss330 <zdoss330@gmail.com>
2026-05-22 17:13:51 -0400

    moved static, added copy static > public dir

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
-from textnode import TextType, TextNode
+import shutil
+import os
+
+def copy_static_to_public(src, dest):
+    
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    os.makedirs(dest)
+
+    copy_directory(src, dest)
+
+def copy_directory(src, dest):
+    for item in os.listdir(src):
+        source_path = os.path.join(src, item)
+        destination_path = os.path.join(dest, item)
+
+        if os.path.isdir(source_path):
+            print(f"Copying directory: {source_path} to {destination_path}")
+            os.makedirs(destination_path)
+
+            copy_directory(source_path, destination_path)
+        else:
+            print(f"Copying file: {source_path} to {destination_path}")
+            shutil.copy(source_path, destination_path)
 
 def main():
-    text_node = TextNode(text="Some great text", text_type=TextType.BOLD, url="https://www.google.com")
-    print(text_node)
+    copy_static_to_public('static', 'public')
 
 main()
